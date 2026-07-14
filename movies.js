9091eddde1fb337518993e6069d84ab808dffed4
extcellent <tkjucker@gmail.com>
2026-07-14 12:19:33 +0200

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,15 @@
+const movieDatabase = [{
+    url: "https://www.youtube.com/watch?v=6_IO-q5X6QQ",
+    title: "Don't Be a Menace",
+    year: "1996",
+    genre: "Comedy",
+    rating: 6.5,
+    desc: "A parody of hood films where a young man navigates life in the hood."
+}, {
+    url: "https://www.youtube.com/watch?v=NEW_VIDEO_ID",
+    title: "New Movie Title",
+    year: "2024",
+    genre: "Action",
+    rating: 8.5,
+    desc: "Movie description..."
+}];
