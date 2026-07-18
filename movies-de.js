fcd44cf989fab5ac42c7f212aca6474e0da8c963
extcellent <tkjucker@gmail.com>
2026-07-18 19:24:42 +0200

    Update movies-de.js

--- movies-de.js
+++ movies-de.js
@@ -598,45 +598,6 @@ const movieDatabase = [
         desc: "A vampire warrior falls for a human caught in the centuries-old war between vampires and werewolves.",
         thumbnail: "https://images5.alphacoders.com/795/thumb-1920-795161.jpg",
         where: []
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=Sh3EFwHg45c",
-        "title": "Safe House",
-        "year": "2012",
-        "genre": "Action, Thriller",
-        "rating": 6.7,
-        "duration": 115,
-        "director": "Daniel Espinosa",
-        "cast": ["Denzel Washington", "Ryan Reynolds", "Vera Farmiga", "Brendan Gleeson", "Sam Shepard"],
-        "desc": "A young CIA agent is tasked with looking after a fugitive in a safe house. But when the safe house is attacked, he finds himself on the run with his charge.",
-        "thumbnail": "https://pixel.disco.nowtv.com/uuid/49dfd8ba-a564-3c91-9314-ad5eb13e7cea/LAND_16_9?language=en-GB&proposition=NOWOTT&version=3547ca51-670c-3a97-bd43-506108d40525",
-        "where": []
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=VD9_0FNSwZA",
-        "title": "Summer Rental",
-        "year": "1985",
-        "genre": "Comedy",
-        "rating": 6.3,
-        "duration": 87,
-        "director": "Carl Reiner",
-        "cast": ["John Candy", "Richard Crenna", "Rip Torn", "Karen Austin", "Kerri Green", "Joey Lawrence"],
-        "desc": "An overworked air traffic controller takes his family on a beach vacation, but a series of mishaps and a feud with a local yachtsman threaten to ruin their summer getaway.",
-        "thumbnail": "",
-        "where": []
-    },
-    {
-    "url": "https://www.youtube.com/watch?v=4oH7uq69sOM",
-    "title": "The Nanny Diaries",
-    "year": "2007",
-    "genre": "Comedy, Drama, Romance",
-    "rating": 6.2,
-    "duration": 106,
-    "director": "Shari Springer Berman, Robert Pulcini",
-    "cast": ["Scarlett Johansson", "Laura Linney", "Paul Giamatti", "Chris Evans", "Alicia Keys", "Donna Murphy"],
-    "desc": "A recent college graduate from a working-class background takes a job as a nanny for a wealthy family on Manhattan's Upper East Side. She must juggle the family's dysfunction, the neglected child in her care, and a new romance, all while trying to figure out her own life path.",
-    "thumbnail": "",
-    "where": []
     }
 ];
 // ============================================================
