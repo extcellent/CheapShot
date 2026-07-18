6fc7ea39783bbee457a58a16ca91a0f4c5e8cf8f
extcellent <tkjucker@gmail.com>
2026-07-18 14:56:51 +0200

    Update movies.js

--- movies.js
+++ movies.js
@@ -611,6 +611,19 @@ const movieDatabase = [
         "desc": "A young CIA agent is tasked with looking after a fugitive in a safe house. But when the safe house is attacked, he finds himself on the run with his charge.",
         "thumbnail": "https://pixel.disco.nowtv.com/uuid/49dfd8ba-a564-3c91-9314-ad5eb13e7cea/LAND_16_9?language=en-GB&proposition=NOWOTT&version=3547ca51-670c-3a97-bd43-506108d40525",
         "where": []
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=VD9_0FNSwZA",
+        "title": "Summer Rental",
+        "year": "1985",
+        "genre": "Comedy",
+        "rating": 6.3,
+        "duration": 87,
+        "director": "Carl Reiner",
+        "cast": ["John Candy", "Richard Crenna", "Rip Torn", "Karen Austin", "Kerri Green", "Joey Lawrence"],
+        "desc": "An overworked air traffic controller takes his family on a beach vacation, but a series of mishaps and a feud with a local yachtsman threaten to ruin their summer getaway.",
+        "thumbnail": "",
+        "where": []
     }
 ];
 // ============================================================
